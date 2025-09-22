6d32aa29439ee3a3ebb26d9b83d7f0ad76c8c673
faith-kerry <kerubofaith944@gmail.com>
2025-09-22 22:55:56 +0300

    worked on my script.js file

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const h2 = document.createElement("h2");
 h2.textcontent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+
+
+// Simple Dark/Light Theme Toggle
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
+  alert("Theme toggled!");
+}
